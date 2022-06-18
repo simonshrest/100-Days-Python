6951dba9853b8b8f09e16704efea4b0dccd6302e
Simon Shrestha <simonshrest@gmail.com>
2022-06-18 04:34:55 +0000

    Average Height Calculator

--- Average Height Calculator/main.py
+++ Average Height Calculator/main.py
@@ -1,21 +1,14 @@
-# x = 0
-# counter = 0
-# average = 0
-# student_heights = input("Input a list of student heights ").split()
-# for n in range(0, len(student_heights)):
-#   student_heights[n] = int(student_heights[n])
-#   x += student_heights[n]
-#   counter += 1
-# average = round((x/counter),0)
-# print(int(average))
-
-
-# 🚨 Don't change the code below 👇
+# Ask user to input the list of student heights
+# Store heights into a list
 student_heights = input("Input a list of student heights ").split()
+# Convert each list item to int
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
-
-
-#Write your code below this row 👇
-print(student_heights)
+# Declare new variable to store sum of heights and also the number of student as couter
+sum_of_heights = 0
+counter = 0
+for x in student_heights:
+    sum_of_heights += x
+    counter += 1
+average = round(sum_of_heights / counter)
+print(average)
