2068fd50e787cbf410b1e143590ca27593dbc99c
Simon Shrestha <simonshrest@gmail.com>
2022-06-18 04:22:34 +0000

    Password Generator

---
+++ Average Height Calculator/main.py
@@ -0,0 +1,21 @@
+# x = 0
+# counter = 0
+# average = 0
+# student_heights = input("Input a list of student heights ").split()
+# for n in range(0, len(student_heights)):
+#   student_heights[n] = int(student_heights[n])
+#   x += student_heights[n]
+#   counter += 1
+# average = round((x/counter),0)
+# print(int(average))
+
+
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+print(student_heights)
